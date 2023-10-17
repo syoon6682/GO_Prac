1658766e1a2bbf5ecc57eda1242cc08ce1a5b143
syoon6682 <dev.syoon6682@gmail.com>
2023-10-17 21:16:07 +0900

    ADD: using gin and make GET

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "pong",
+        })
+    })
+    r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
